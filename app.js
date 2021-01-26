6f9c9b8348ceb93c95f5ae8818c50460dfdc6d9e
ishanmazhar <ishanmazhar2@gmail.com>
2021-01-26 23:58:39 +0600

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list'); 
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo); 
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button'); 
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement('button'); 
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton); 
+
+    //APPEND TODO-DIV TO TODO-LIST
+    todoList.appendChild(todoDiv);
+
+    //CLEAR TOTO INPUT VALUE
+    todoInput.value = '';
+
+
+}
